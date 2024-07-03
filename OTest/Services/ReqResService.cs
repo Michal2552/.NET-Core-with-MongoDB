019712e7cbbead0af8bd9d9ec84cb68bd9d1a7c3
Michal Golombek <87675953+Michal2552@users.noreply.github.com>
2024-07-04 02:05:56 +0300

    Update ReqResService.cs

--- OTest/Services/ReqResService.cs
+++ OTest/Services/ReqResService.cs
@@ -52,26 +52,26 @@ namespace OTest.Services
             }
         }
 
-        //public async Task<string> LoginUser(string email, string password)
-        //{
-        //    var user = new { email = email, password = HashPassword(password) };
-        //    HttpResponseMessage response = await _httpClient.PostAsJsonAsync("api/login", user);
-        //    response.EnsureSuccessStatusCode();
-        //    return await response.Content.ReadAsStringAsync();
-        //}
+        public async Task<string> LoginUser(string email, string password)
+        {
+            var user = new { email = email, password = HashPassword(password) };
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("api/login", user);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
 
-        //private string HashPassword(string password)
-        //{
-        //    using (SHA256 sha256Hash = SHA256.Create())
-        //    {
-        //        byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
-        //        StringBuilder builder = new StringBuilder();
-        //        for (int i = 0; i < bytes.Length; i++)
-        //        {
-        //            builder.Append(bytes[i].ToString("x2"));
-        //        }
-        //        return builder.ToString();
-        //    }
-        //}
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
     }
 }
